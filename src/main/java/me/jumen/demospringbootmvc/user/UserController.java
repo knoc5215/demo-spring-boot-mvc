ac9341b99a1c13b7e30c1c4bfa603cddb5718d96
knoc5215 <knoc5215@naver.com>
2020-12-23 21:55:31 +0900

    스프링 웹 MVC 2부: HttpMessageConverters

--- src/main/java/me/jumen/demospringbootmvc/user/UserController.java
+++ src/main/java/me/jumen/demospringbootmvc/user/UserController.java
@@ -1,13 +1,19 @@
 package me.jumen.demospringbootmvc.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController // @ResponseBody를 갖고있음
 public class UserController {
 
+    //@ResponseBody를 생략할 수 있다 -> StringMessageConverter
+    //@Controller + @ResponseBody가 없는 경우 -> return string을 BeanNameViewResolver string에 해당하는 view를 찾으려고 한다
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/users/create")
+    public User create(@RequestBody User user) {
+        return user;
     }
 }
--- src/test/java/me/jumen/demospringbootmvc/user/UserControllerTest.java
+++ src/test/java/me/jumen/demospringbootmvc/user/UserControllerTest.java
@@ -3,11 +3,13 @@ package me.jumen.demospringbootmvc.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /*
 autoconfigure.jar -> spring.factories 참고
@@ -23,6 +25,19 @@ public class UserControllerTest {
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"));
+
+    }
+
+    @Test
+    public void createUser_JSON() throws Exception {
+        String userJson = "{\"username\":\"jumen\", \"password\":\"5215\"}";
+        mockMvc.perform(post("/users/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept((MediaType.APPLICATION_JSON))
+                .content(userJson))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.username", is(equalTo("jumen"))))
+            .andExpect(jsonPath("$.password", is(equalTo("5215"))));
 
     }
 }
