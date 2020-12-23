74a02784683962cf82e0baafb12d3f1c8a4555c2
knoc5215 <knoc5215@naver.com>
2020-12-23 22:20:28 +0900

    스프링 웹 MVC 4부: 정적 리소스 지원 - WebMvcConfigurer - addResourceHandlers()

--- src/main/java/me/jumen/demospringbootmvc/config/WebConfig.java
+++ src/main/java/me/jumen/demospringbootmvc/config/WebConfig.java
@@ -1,8 +1,24 @@
 package me.jumen.demospringbootmvc.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    /*
+    spring boot가 제공하는 기본 리소스 핸들러
+    classpath:/static
+    classpath:/public
+    classpath:/resources/
+    classpath:/META-INF/resources
+    를 유지하면서, 커스텀 핸들러를 추가하고 싶으면 사용하는 것
+    캐싱전략
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")    // 기존 핸들러에 추가하는 것
+                .addResourceLocations("classpath:/m/")      // 반드시 /로 끝나야함
+                .setCachePeriod(20);
+    }
 }
