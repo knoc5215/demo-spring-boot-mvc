e4d1b4731a193eaae5e027c940339576d9562954
knoc5215 <knoc5215@naver.com>
2020-12-24 11:29:23 +0900

    스프링 웹 MVC 8부: HtmlUnit

--- src/test/java/me/jumen/demospringbootmvc/sample/SampleControllerTest.java
+++ src/test/java/me/jumen/demospringbootmvc/sample/SampleControllerTest.java
@@ -1,14 +1,16 @@
 package me.jumen.demospringbootmvc.sample;
 
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlHeading1;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -28,6 +30,16 @@ class SampleControllerTest {
                 .andExpect(view().name("hello"))
                 .andExpect(model().attribute("name", is("jumen")))
                 .andExpect(content().string(containsString("jumen")));
+    }
+
+    @Autowired
+    WebClient webClient;    // html을 전문적으로 테스트하고 싶다?
+
+    @Test
+    public void htmlUnit() throws Exception {
+        HtmlPage page = webClient.getPage("/hello");
+        HtmlHeading1 h1 = page.getFirstByXPath("//h1");
+        assertThat(h1.getTextContent()).isEqualToIgnoringCase("jumen");
     }
 
 }
